a944040c683a5b05c4d877daeda828420c788a38
lukewhite32 <113476319+lukewhite32@users.noreply.github.com>
2023-01-05 17:52:19 -0500

    Update BaseMotor.hpp

--- motor/BaseMotor.hpp
+++ motor/BaseMotor.hpp
@@ -4,11 +4,15 @@
 class BaseMotor {
 public:
     virtual void SetPercent(double percent) = 0;
+    virtual void setInverted() = 0;
     virtual void SetP(double kP) = 0;
     virtual void SetI(double kI) = 0;
     virtual void SetD(double kD) = 0;
     virtual void SetF(double kF) = 0;
     virtual void SetOutputRange(double kPeakOF, double kPeakOR, double kNominalOF, double kNominalOR) = 0;
+    virtual double GetPosition() = 0;
+    virtual double GetVelocity() = 0;
     virtual void SetPositionPID(double position) = 0;
     virtual void SetSpeedPID(double speed) = 0;
+    virtual bool isAtZero() = 0;
 };
