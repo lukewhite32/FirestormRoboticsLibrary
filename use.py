ee9b78c13a5429c0b55d997d5ab3494cb33cfd2f
Lazy Hippopotamus <lazy@hippo.com>
2023-01-19 21:21:24 -0500

    cool stuff

--- use.py
+++ use.py
@@ -1,4 +1,14 @@
 from requests import get
+import os
 
 
 r = get("https://firestorm-robotics-code.github.io/FirestormRoboticsLibrary/exports", timeout=1)
+try:
+    os.mkdir("src/main/include/FRL")
+except:
+    pass
+for x in r.text.split("\n"):
+    if len(x) == 0:
+        continue
+    if x[0] == "D":
+        os.mkdir("src/main/include/FRL/" + x[1:])
