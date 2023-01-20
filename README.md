5679c90ce4b19a00475a40273a42936ba5a71ee6
Lazy Hippopotamus <lazy@hippo.com>
2023-01-19 21:31:48 -0500

    finalize use.py?

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Add
+To use:
+Download [use.py](https://firestorm-robotics-code.github.io/FirestormRoboticsLibrary/use.py) and place it in your robot project's root directory (NOT under src!) and add
 ```
 task loadFirestormRoboticsLibrary(type: Exec) {
     commandLine 'python', 'use.py'
@@ -6,7 +7,8 @@ task loadFirestormRoboticsLibrary(type: Exec) {
 
 build.dependsOn loadFirestormRoboticsLibrary
 ```
-to `build.gradle` in your robot project. `use.py` will automatically check the library version and download it if necessary every time a build runs on wifi.  
+to `build.gradle` in your robot project.  
+`use.py` will automatically check the library version and download it if necessary every time a build runs on wifi.  
 From there, just `#include <FRL/whatever/whatever.hpp>` to use libraries.
 
 Everything is kept under the namespace `frl` - theoretically. In practice almost nothing is. This is a Work In Progress.
--- use.py
+++ use.py
@@ -7,8 +7,18 @@ try:
     os.mkdir("src/main/include/FRL")
 except:
     pass
-for x in r.text.split("\n"):
-    if len(x) == 0:
-        continue
-    if x[0] == "D":
-        os.mkdir("src/main/include/FRL/" + x[1:])
+if r.status_code == 200:
+    for x in r.text.split("\n"):
+        if len(x) == 0:
+            continue
+        if x[0] == "D":
+            try:
+                os.mkdir("src/main/include/FRL/" + x[1:])
+            except:
+                pass
+        if x[0] == "F":
+            f = get("https://firestorm-robotics-code.github.io/FirestormRoboticsLibrary/" + x[1:])
+            if f.status_code == 200:
+                open("src/main/include/FRL/" + x[1:], "w+").write(f.text)
+            else:
+                print("Couldn't download", x[1:] + "!", "This is probably because somebody wrote a malformed dependency line. Please submit a bug report.\nNote: This is not a breaking issue; if your code (or another file you're using) doesn't depend on", x[1:], "you're fine.")
